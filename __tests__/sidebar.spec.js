964411f74a7921e37c83c3a3ea6edf44424bd9b0
G1enY0ung <g1enyy0ung@gmail.com>
2017-05-13 11:14:24 +0800

    enhance Sidebar test

--- __tests__/sidebar.spec.js
+++ __tests__/sidebar.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { Sidebar } from '../src/components/Sidebar/index.jsx'
 
 describe('<Sidebar />', () => {
@@ -12,5 +12,14 @@ describe('<Sidebar />', () => {
     wrapper.find(NavLink).forEach(l => {
       expect(l.props().to).toBe(toArray[index++])
     })
+  })
+
+  test('navlink active class', () => {
+    expect(wrapper.find(NavLink).first().props().activeClassName).toBe('selected')
+  })
+
+  test('login navlink change', () => {
+    let wrapperWithId = shallow(<Sidebar id={1} />)
+    expect(wrapperWithId.find(Link).props().to).toMatch('/personal/recent')
   })
 })
