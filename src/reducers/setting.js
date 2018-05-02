4d68e73ee92473d7bd010aa86476670ca8b34068
g1eny0ung <g1enyy0ung@gmail.com>
2018-05-02 13:17:05 +0800

    Release 2.7.0

--- src/reducers/setting.js
+++ src/reducers/setting.js
@@ -4,7 +4,7 @@ import * as types from '../actions/setting/types'
 const settingReducer = (
   state = {
     mainVersion: 2,
-    secondaryVersion: 6,
+    secondaryVersion: 7,
     volume: 20,
     saveSuccess: false
   },
