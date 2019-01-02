bb97702e2b0528c02de9c772fd186003cae46865
Yue Yang <g1enyy0ung@gmail.com>
2019-01-02 12:57:37 +0800

    Release 2.14.0

--- src/reducers/setting.js
+++ src/reducers/setting.js
@@ -4,7 +4,7 @@ import * as types from '../actions/setting/types'
 const settingReducer = (
   state = {
     mainVersion: 2,
-    secondaryVersion: 13,
+    secondaryVersion: 14,
     volume: 20,
     openWithPlaying: true,
     restoreLastWinPos: false,
