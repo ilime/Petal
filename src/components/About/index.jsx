6431f5d7ddd2c68e251d80a7c5c27c49e3fcc93a
Yue Yang <g1enyy0ung@gmail.com>
2019-04-25 11:36:00 +0800

    Change about text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -24,41 +24,58 @@ export default class About extends Component {
               onClick={openInDefaultBrowser('https://github.com/ilime/Petal')}
             >
               Petal
-            </span> 是一个豆瓣FM第三方客户端。
-          </p>
-          <p>应用的所有功能均使用豆瓣FM移动版API构建。</p>
-          <p>
-            Petal 的诞生离不开<span
-              onClick={openInDefaultBrowser('https://douban.fm')}
-            >
+            </span>{' '}
+            是一个
+            <span onClick={openInDefaultBrowser('https://douban.fm')}>
               豆瓣FM
-            </span>。
+            </span>
+            第三方客户端。
+          </p>
+          <p>
+            应用的所有功能均使用
+            <span onClick={openInDefaultBrowser('https://douban.fm')}>
+              豆瓣FM
+            </span>
+            的 Android/iOS APP 提供的 API 构建。
+          </p>
+          <p>
+            Petal 的诞生离不开
+            <span onClick={openInDefaultBrowser('https://douban.fm')}>
+              豆瓣FM
+            </span>
+            。
           </p>
           <p>不得将此应用用于任何不正当用途。</p>
           <p>
-            Petal 的维护者是<span
+            Petal 的维护者是{' '}
+            <span
               onClick={openInDefaultBrowser('https://github.com/g1eny0ung')}
             >
               g1eny0ung
-            </span>。
+            </span>{' '}
+            。
           </p>
           <p>
-            如果有任何使用上的问题，请前往<span
+            如果有任何使用上的问题，请前往
+            <span
               onClick={openInDefaultBrowser(
                 'https://github.com/ilime/Petal/issues'
               )}
             >
-              仓库下的 issues
-            </span> 下提出。
+              代码仓库的 issues
+            </span>{' '}
+            下提出。
           </p>
           <p>
-            使用 <span
+            使用{' '}
+            <span
               onClick={openInDefaultBrowser(
                 'https://opensource.org/licenses/MIT'
               )}
             >
               MIT
-            </span> 许可证。
+            </span>{' '}
+            许可证。
           </p>
           <p>Copyright (c) 2019 ilime。</p>
         </div>
