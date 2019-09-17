eee6088a2941add7ae1e112ab65c483f0da0d5a0
Yue Yang <g1enyy0ung@gmail.com>
2019-09-17 18:49:25 +0800

    Update maintainers

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -20,62 +20,36 @@ export default class About extends Component {
         <Image src="./resources/petal.svg" size="tiny" centered />
         <div className="content">
           <p>
-            <span
-              onClick={openInDefaultBrowser('https://github.com/ilime/Petal')}
-            >
-              Petal
-            </span>{' '}
-            是一个
-            <span onClick={openInDefaultBrowser('https://douban.fm')}>
-              豆瓣FM
-            </span>
+            <span onClick={openInDefaultBrowser('https://github.com/ilime/Petal')}>Petal</span> 是一个
+            <span onClick={openInDefaultBrowser('https://douban.fm')}>豆瓣FM</span>
             第三方客户端。
           </p>
           <p>
             应用的所有功能均使用
-            <span onClick={openInDefaultBrowser('https://douban.fm')}>
-              豆瓣FM
-            </span>
-            的 Android/iOS APP 提供的 API 构建。
+            <span onClick={openInDefaultBrowser('https://douban.fm')}>豆瓣FM</span>的 Android/iOS APP 提供的 API 构建。
           </p>
           <p>
             Petal 的诞生离不开
-            <span onClick={openInDefaultBrowser('https://douban.fm')}>
-              豆瓣FM
-            </span>
-            。
+            <span onClick={openInDefaultBrowser('https://douban.fm')}>豆瓣FM</span>。
           </p>
           <p>不得将此应用用于任何不正当用途。</p>
+          <p>Petal 的维护者是：</p>
           <p>
-            Petal 的维护者是{' '}
-            <span
-              onClick={openInDefaultBrowser('https://github.com/g1eny0ung')}
-            >
-              g1eny0ung
-            </span>{' '}
-            。
+            <span onClick={openInDefaultBrowser('https://github.com/g1eny0ung')}>g1eny0ung</span>
+          </p>
+          <p>
+            <span onClick={openInDefaultBrowser('https://github.com/Rapiz1')}>Rapiz1</span>
+          </p>
+          <p>
+            <span onClick={openInDefaultBrowser('https://github.com/lxs137')}>lxs137</span>
           </p>
           <p>
             如果有任何使用上的问题，请前往
-            <span
-              onClick={openInDefaultBrowser(
-                'https://github.com/ilime/Petal/issues'
-              )}
-            >
-              代码仓库的 issues
-            </span>{' '}
+            <span onClick={openInDefaultBrowser('https://github.com/ilime/Petal/issues')}>代码仓库的 issues</span>{' '}
             下提出。
           </p>
           <p>
-            使用{' '}
-            <span
-              onClick={openInDefaultBrowser(
-                'https://opensource.org/licenses/MIT'
-              )}
-            >
-              MIT
-            </span>{' '}
-            许可证。
+            使用 <span onClick={openInDefaultBrowser('https://opensource.org/licenses/MIT')}>MIT</span> 许可证。
           </p>
           <p>Copyright (c) 2019 ilime。</p>
         </div>
